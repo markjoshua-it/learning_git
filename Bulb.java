3d53547da3d89ffac6d2a3797750e09eb5971af5
Joshua Padilla <markjoshuapadilla.it@gmail.com>
2025-09-24 23:32:06 +0800

    modify Image dir for universal dir

--- Bulb.java
+++ Bulb.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class Bulb extends JCheckBox {
 	public Bulb(){
-		setIcon(new ImageIcon("assets/bulb_off.png"));
-		setSelectedIcon(new ImageIcon("assets/bulb_on.png"));
+		setIcon(new ImageIcon("src/learning_git/assets/bulb_off.png"));
+		setSelectedIcon(new ImageIcon("src/learning_git/assets/bulb_on.png"));
 		setOpaque(false);
 	}
 }
